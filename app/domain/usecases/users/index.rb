56fc899c40fd6d2d67fb252ec6cfd85cc5edad9b
George borsato <georginhoborsato@gmail.com>
2024-03-11 20:47:22 -0400

    remove files

--- app/domain/usecases/users/index.rb
+++
@@ -1,9 +0,0 @@
-module Users
-  class Index < ::Micro::Case
-    attributes :name, :email, :password
-
-    def call!
-      return Failure(:invalid_name) if name.nil? || name.empty?
-    end
-  end
-end
